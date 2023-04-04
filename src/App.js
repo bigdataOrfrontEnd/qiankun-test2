d3b4c680ee2e3a8724ecb6272e216706ef2e5c93
17639638825 <562985990@qq.com>
2023-04-04 10:17:00 +0800

    简洁子应用

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/index";
+
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Link to="hom">首页</Link>
-        <Link to="/my">我得</Link>
-        <Routes>
-          <Route path="/hom" element={<Home />}></Route>
-          {/* <Route path="/my" element={<My />}></Route> */}
-        </Routes>
-      </Router>
-    </div>
-  );
+  return <div className="App">111</div>;
 }
 
 export default App;
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function Home() {
-  return <div>主页面</div>;
-}
-export default Home;
