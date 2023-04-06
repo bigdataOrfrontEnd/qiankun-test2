90991eac8dec041d0607a0f5ff3b764b56230284
17639638825 <562985990@qq.com>
2023-04-06 15:00:27 +0800

    样式修改基本完毕

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,20 +1,47 @@
 // 登录页面
-import React from "react";
+import { React, useState } from "react";
 import "./index.scss";
-import { Form, Input, Button, Radio } from "antd";
+import { Form, Input, Button, Radio, Modal } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 export default function Login() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div className="loginbg">
       <h1>我们的实验田</h1>
       <div className="main">
         <h2>登录</h2>
         <Form>
-          <Input placeholder="请输入用户名"></Input>
-          <Input type="password" placeholder="请输入密码"></Input>
-          <Radio>请认证阅读乡姑纳规定----应该是一个弹框</Radio>
-          <Button> 登录</Button>
+          <Input placeholder="请输入用户名" prefix={<UserOutlined />}></Input>
+          <Input
+            type="password"
+            placeholder="请输入密码"
+            prefix={<UserOutlined />}
+            autoComplete="off"
+          ></Input>
+          <Radio>
+            <Button type="text" onClick={showModal}>
+              请认证阅读乡姑纳规定----应该是一个弹框
+            </Button>
+          </Radio>
+          <br />
+          <Button type="primary"> 登录</Button>
         </Form>
       </div>
+      <Modal
+        title="详细信息"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      ></Modal>
     </div>
   );
 }
