38feaf1f69bbe5460aaf0fb8a6cc89536115ed14
17639638825 <562985990@qq.com>
2023-04-12 11:23:15 +0800

    删除评论

--- src/pages/demo/one/one.js
+++ src/pages/demo/one/one.js
@@ -17,6 +17,8 @@ export default class one extends Component {
       { id: 4, name: "刘德妈的话华", content: "冰雨" },
       { id: 5, name: "刘德华", content: "冰雨" },
     ],
+    name: "",
+    content: "",
   };
   render() {
     return (
@@ -44,9 +46,18 @@ export default class one extends Component {
         <li key={item.id}>
           <h1>评论人：{item.name}</h1>
           <p>评论内容：{item.content}</p>
-          <button>删除</button>
+          {/* <button onClick={this.del.bind(this, item.id)}>删除</button> */}
+          <button onClick={() => this.del(item.id)}>删除</button>
         </li>
       ));
     }
   }
+  //删除每条评论，需要传递参数
+  del = (id) => {
+    console.log(id);
+    this.setState({
+      list: this.state.list.filter((item) => item.id !== id),
+    });
+  };
+  //发表评论，并清空评论输入的地方
 }
