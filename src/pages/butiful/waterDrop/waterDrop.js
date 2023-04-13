55156273e0b21c36737829c4132137e155e74fe5
17639638825 <562985990@qq.com>
2023-04-13 10:45:08 +0800

    添加内容区域

--- src/pages/butiful/waterDrop/waterDrop.js
+++ src/pages/butiful/waterDrop/waterDrop.js
@@ -3,9 +3,27 @@ import "./water.scss";
 export default function WaterDrop() {
   return (
     <div className="container">
-      <div className="drop"></div>
-      <div className="drop"></div>
-      <div className="drop"></div>
+      <div className="drop">
+        <div className="content">
+          <h2>01</h2>
+          <p>学了一些好东西</p>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+      <div className="drop">
+        <div className="content">
+          <h2>01</h2>
+          <p>学了一些好东西</p>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+      <div className="drop">
+        <div className="content">
+          <h2>01</h2>
+          <p>学了一些好东西</p>
+          <a href="#">Read More</a>
+        </div>
+      </div>
     </div>
   );
 }
