96d933c282fd8d9498ac279599bcc95657f2ef48
17639638825 <562985990@qq.com>
2023-04-14 14:21:05 +0800

    提交

--- src/pages/butiful/two/index.js
+++ src/pages/butiful/two/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import "./index.scss";
+import hill1 from "@/assets/two/hill1.png";
 export default function HomeLout() {
-  return <div>index</div>;
+  return <div></div>;
 }
