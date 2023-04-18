bc73003c17046ee005e608a61406773a342cf6ab
17639638825 <562985990@qq.com>
2023-04-18 13:25:01 +0800

    存储到本地

--- src/pages/demo/todoMvc/todo/index.js
+++ src/pages/demo/todoMvc/todo/index.js
@@ -6,14 +6,23 @@ import TodoHeader from "./components/TodoHeader";
 import TodoMain from "./components/TodoMain";
 import TodoFooter from "./components/TodoFooter";
 export default class TodoStyle extends Component {
+  // { id: "1", name: "小明", done: false },
+  //     { id: "2", name: "侠岚", done: true },
+  //     { id: "3", name: "小风", done: false },
   state = {
-    listDate: [
-      { id: "1", name: "小明", done: false },
-      { id: "2", name: "侠岚", done: true },
-      { id: "3", name: "小风", done: false },
-    ],
+    listDate: [],
     type: "all",
   };
+  // 走本地读取
+  componentDidMount() {
+    this.setState({
+      listDate: JSON.parse(localStorage.getItem("todo")) || [],
+    });
+  }
+  //村本地
+  componentDidUpdate() {
+    localStorage.setItem("todo", JSON.stringify(this.state.listDate));
+  }
 
   render() {
     const { listDate, type } = this.state;
